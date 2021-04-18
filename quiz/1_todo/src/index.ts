2280f202cc15978cd0bb414d7974c841ade90e1b
bulhwi <mm225012@gmail.com>
2021-04-19 02:38:09 +0900

    타입스크립트 정리

---
+++ quiz/1_todo/src/index.ts
@@ -0,0 +1,74 @@
+// type 별칭
+// type Todo = {
+//   id: number;
+//   title: string;
+//   done: boolean;
+// };
+
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+let todoItems: Todo[];
+
+// api
+function fetchTodoItems(): Todo[] {
+  const todos = [
+    { id: 1, title: '안녕', done: false },
+    { id: 2, title: '타입', done: false },
+    { id: 3, title: '스크립트', done: false },
+  ];
+  return todos;
+}
+
+// crud methods
+function fetchTodos(): object[] {
+  const todos = fetchTodoItems();
+  return todos;
+}
+
+function addTodo(todo: Todo): void {
+  todoItems.push(todo);
+}
+
+function deleteTodo(index: number): void {
+  todoItems.splice(index, 1);
+}
+
+function completeTodo(index: number, todo: Todo): void {
+  todo.done = true;
+  todoItems.splice(index, 1, todo);
+}
+
+// business logic
+function logFirstTodo(): object {
+  return todoItems[0];
+}
+
+function showCompleted(): object[] {
+  return todoItems.filter(item => item.done);
+}
+
+function addTwoTodoItems(): void {
+  const item1 = {
+    id: 4,
+    title: 'item 4',
+    done: false,
+  };
+  addTodo(item1);
+  addTodo({
+    id: 5,
+    title: 'item 5',
+    done: false,
+  });
+}
+
+// NOTE: 유틸 함수
+function log(): void {
+  console.log(todoItems);
+}
+
+todoItems = fetchTodoItems();
+addTwoTodoItems();
+log();
