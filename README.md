38533569cca246401ce9257bf3fee668b278ac41
bulhwi <mm225012@gmail.com>
2021-04-28 23:01:27 +0900

    유틸리티타입 Pick, Partial, Omit 정리

--- README.md
+++ README.md
@@ -19,3 +19,4 @@ npm install typescript -g
 * [좋은 소프트웨어는 확장이 용이해야 한다는 원칙의 위키 글](https://en.wikipedia.org/wiki/Open%E2%80%93closed_principle)
 * [타입스크립트의 모듈화](https://joshua1988.github.io/ts/usage/modules.html)
 * [ES6 Modules](https://joshua1988.github.io/vue-camp/es6/modules.html#%EB%AA%A8%EB%93%88%ED%99%94%EC%9D%98-%ED%95%84%EC%9A%94%EC%84%B1)
+* [타입스크립트 공식 사이트의 유틸리티 타입 문서](https://www.typescriptlang.org/docs/handbook/utility-types.html)
--- class-note/13_Utility-type.ts
+++ class-note/13_Utility-type.ts
@@ -19,8 +19,72 @@ interface ProductDetail {
   price: number;
 }
 
-// type ShoppingItem = Pick<Product, 'id'|'name'|'price'>;
-
+type ShoppingItem = Pick<Product, 'id'|'name'|'price'>;
 function displayProductDetail(shoppingItem: Pick<Product, 'id'|'name'|'price'>) {
   // ..
+}
+
+// Partial - 오브젝트의 요소를 옵셔널하게 처리.
+// interface UpdateProduct {
+//   id?: number;
+//   name?: string;
+//   price?: number;
+//   brand?: string;
+//   stock?: number;
+// }
+// type UpdateProduct = Partial<Product>
+function updateProductItem(productItem: Partial<Product>) {
+ //....
+}
+
+
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+
+// Partial 구현1
+// interface UserProfileUpdate {
+//   username?: string;
+//   email?: string;
+//   profilePhotoUrl?: string;
+// }
+
+// Partial 구현2
+// type UserProfileUpdate = {
+//   username?: UserProfile['username'];
+//   email?: UserProfile['email'];
+//   profilePhotoUrl?: UserProfile['profilePhotoUrl'];
+// }
+
+// Partial 구현3
+// type UserProfileUpdate = {
+//     [p in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[p]
+// }
+
+// Partial 구현4
+type userProfileKeys = keyof UserProfile;
+type UserProfileUpdate = {
+  [p in keyof UserProfile]?: UserProfile[p]
+}
+
+// Partial 구현5
+type Subset<T> = {
+  [p in keyof T]?: T[p]
+}
+
+// Pick의 반대가 Omit
+// Omit
+interface AddressBook {
+  name: string;
+  phone: number;
+  address: string;
+  company: string;
+}
+
+const phoneBook: Omit<AddressBook, "address"> = {
+  name: "name",
+  phone: 123456789,
+  company: "company"
 }
